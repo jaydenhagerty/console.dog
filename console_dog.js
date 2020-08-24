a424eedbdb7099f0e3debfdd5c1a1057641d7fbf
jaydenhagerty <technocrat0505@gmail.com>
2020-08-24 12:37:54 -0700

    this is the first commit

---
+++ console_dog.js
@@ -0,0 +1,7 @@
+// ---- Console Dog v1.4 created by Jayden H ---- 
+
+console.dog = function (input) {
+    if (input == "!bark") console.log("WOOF WOOF! I AM A DOG!");
+    else if (input == "!gifs") window.open('https://giphy.com/explore/dog');
+    else console.log(input);
+}
